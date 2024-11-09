3f055eaf65079590c35a1110ea4a4656e1d10c3c
vityshha <vitysha.yakunin@gmail.com>
2024-11-09 18:32:31 +0300

    add metrics

--- cores/0. core_search_helper.py
+++ cores/0. core_search_helper.py
@@ -5,37 +5,44 @@ import glob
 from tqdm import tqdm
 import os
 
-
-
 PATH = '/Users/vityshha/Desktop/Мегакариоциты (клетки и ядра)/Мегакариоциты (только клетки)/'
 PATH_CORES = '/Users/vityshha/Desktop/Мегакариоциты (клетки и ядра)/Мегакариоциты (клетки и ядра)/'
 
+PATH_SAVE = './cores_contours/'
+
+NEED_SHOW_RESULT = True
+NEED_SAVE_RESULT = False
+
+os.makedirs(PATH_SAVE, exist_ok=True)
 
 # Функция для поиска и выделения контуров ядер на изображении клетки
 def find_core_contour_in_cell(cell_image, core_template, offset=(0, 0)):
     core_gray = cv2.cvtColor(core_template, cv2.COLOR_BGR2GRAY)
-    _, core_binary = cv2.threshold(core_gray, 240, 255, cv2.THRESH_BINARY_INV)
+    _, core_binary = cv2.threshold(core_gray, 250, 255, cv2.THRESH_BINARY_INV)
 
     contours, _ = cv2.findContours(core_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Создаем пустую маску для хранения контуров
+    mask = np.zeros(cell_image.shape[:2], dtype=np.uint8)
     for contour in contours:
         shifted_contour = contour + np.array([offset[0], offset[1]])
+        cv2.drawContours(mask, [shifted_contour], -1, 255, thickness=cv2.FILLED)
         cv2.drawContours(cell_image, [shifted_contour], -1, (255, 0, 0), 1)
 
-    return cell_image
+    return cell_image, mask
 
 
 # Функция для поиска нескольких ядер и их отображения
-def process_image_with_cores(cell_image, core_path_base):
+def process_image_with_cores(cell_image, core_path_base, image_name):
     result_image = cell_image.copy()
     suffix = ""
     core_index = 1
+    full_mask = np.zeros(cell_image.shape[:2], dtype=np.uint8)
 
     # Пробуем загрузить шаблоны с индексами (1), (2), ...
     while True:
         core_path = f"{core_path_base}{suffix}.tif"
 
-        # Проверяем, существует ли файл ядра
         if os.path.exists(core_path):
             core_template = cv2.imread(core_path)
 
@@ -45,7 +52,8 @@ def process_image_with_cores(cell_image, core_path_base):
             result = cv2.matchTemplate(cell_gray, core_gray, cv2.TM_CCOEFF_NORMED)
             _, _, _, max_loc = cv2.minMaxLoc(result)
 
-            result_image = find_core_contour_in_cell(result_image, core_template, offset=max_loc)
+            result_image, mask = find_core_contour_in_cell(result_image, core_template, offset=max_loc)
+            full_mask = cv2.bitwise_or(full_mask, mask)
 
             suffix = f"({core_index})"
             core_index += 1
@@ -59,27 +67,35 @@ def process_image_with_cores(cell_image, core_path_base):
             else:
                 break
 
-    return result_image
+    contour_save_path = os.path.join(PATH_SAVE, f"{image_name}_contours.png")
+    cv2.imwrite(contour_save_path, full_mask)
+
+    return result_image, full_mask
 
 
 for file_path in tqdm(glob.glob(PATH + '/*.tif'), desc="Processing images"):
     image_name = file_path.split('/')[-1].split('.tif')[0]
-    print("Processing:", image_name)
 
     cell_image = cv2.imread(file_path)
     cell_image = cv2.cvtColor(cell_image, cv2.COLOR_BGR2RGB)
 
     core_path_base = PATH_CORES + file_path.split('/')[-1].replace('_S', '_N').replace('.tif', '')
 
-    result_image = process_image_with_cores(cell_image, core_path_base)
+    result_image, full_mask = process_image_with_cores(cell_image, core_path_base, image_name)
 
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.title("Оригинальное")
-    plt.imshow(cell_image)
+    if NEED_SHOW_RESULT:
 
-    plt.subplot(1, 2, 2)
-    plt.title("Разметка специалиста")
-    plt.imshow(result_image)
+        plt.figure(figsize=(15, 5))
+        plt.subplot(1, 3, 1)
+        plt.title("Оригинальное")
+        plt.imshow(cell_image)
 
-    plt.show()
+        plt.subplot(1, 3, 2)
+        plt.title("Разметка специалиста")
+        plt.imshow(result_image)
+
+        plt.subplot(1, 3, 3)
+        plt.title("Маска контуров ядер")
+        plt.imshow(full_mask, cmap='gray')
+
+        plt.show()
--- cores/2. classic_kmeans_detect.py
+++ cores/2. classic_kmeans_detect.py
@@ -8,59 +8,70 @@ import os
 
 PATH = '/Users/vityshha/Desktop/Мегакариоциты (клетки и ядра)/Мегакариоциты (только клетки)/'
 PATH_CORES = '/Users/vityshha/Desktop/Мегакариоциты (клетки и ядра)/Мегакариоциты (клетки и ядра))/'
+PATH_CORES_CONTOURS = './cores_contours/'
 
 PATH_SAVE = './classic_kmeans_detect_out'
 NEED_SAVE_RESULTS = False
 NEED_SHOW_RESULT = True
+NEED_TEST = False
 
 MIN_AREA = 100
 MAX_AREA_COEFF = 0.9
+total_dice, total_iou = 0, 0
+processed_images = 0
+CLASSES = 3
 NEED_USE_MEAN_SHIFT = True
 
-for file_path in tqdm(glob.glob(PATH + '/*.tif'), desc="Processing images"):
+def calculate_metrics(pred_mask, true_mask):
+    pred_mask = (pred_mask > 0).astype(np.uint8)
+    true_mask = (true_mask > 0).astype(np.uint8)
+
+    intersection = np.logical_and(pred_mask, true_mask).sum()
+    union = np.logical_or(pred_mask, true_mask).sum()
+    dice_score = 2 * intersection / (pred_mask.sum() + true_mask.sum()) if (pred_mask.sum() + true_mask.sum()) != 0 else 0
+    iou_score = intersection / union if union != 0 else 0
+    return dice_score, iou_score
+
+for file_path in tqdm(glob.glob(PATH + '/*.tif')):
     image_name = file_path.split('/')[-1].split('.tif')[0]
-    print(image_name)
+
     image = cv2.imread(file_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Обрабатываем ситуацию когда контура во всю клетку
+    # Обрабатываем ситуацию когда контуры по всей клетке
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, binary_mask = cv2.threshold(image_gray, 250, 255, cv2.THRESH_BINARY_INV)
     contours_object, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     area = cv2.contourArea(contours_object[0])
     MAX_AREA = area * MAX_AREA_COEFF
 
-    image_shift = image
+    # NEED_USE_MEAN_SHIFT
+    image_shift = cv2.pyrMeanShiftFiltering(image, sp=40, sr=20) if NEED_USE_MEAN_SHIFT else image
+    pixel_values = np.float32(image_shift.reshape((-1, 3)))
 
-    if NEED_USE_MEAN_SHIFT:
-        image_shift = cv2.pyrMeanShiftFiltering(image, sp=40, sr=20)
-
-    pixel_values = image_shift.reshape((-1, 3))
-    pixel_values = np.float32(pixel_values)
-
+    # KMEANS
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 5, 10)
-    k = 3
-
-    _, labels, centers = cv2.kmeans(pixel_values, k, None, criteria, 20, cv2.KMEANS_RANDOM_CENTERS)
-
+    _, labels, centers = cv2.kmeans(pixel_values, CLASSES, None, criteria, 20, cv2.KMEANS_RANDOM_CENTERS)
     centers = np.uint8(centers)
+    segmented_image = centers[labels.flatten()].reshape(image_shift.shape)
 
-    segmented_image = centers[labels.flatten()]
-    segmented_image = segmented_image.reshape(image_shift.shape)
-
-    # Определение самого темного кластера по яркости
+    # Берем тот класс, который самый темный (является ядром)
     brightness = np.mean(centers, axis=1)
     selected_class = np.argmin(brightness)
+    mask_image = (labels.flatten() == selected_class).astype(np.uint8).reshape(image_shift.shape[:2]) * 255
 
-    mask = np.zeros(labels.shape, dtype=np.uint8)
-    mask[labels.flatten() == selected_class] = 255
-    mask_image = mask.reshape(image_shift.shape[0], image_shift.shape[1])
-
+    # Морфологические операции
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     mask_image = cv2.morphologyEx(mask_image, cv2.MORPH_OPEN, kernel, iterations=2)
     mask_image = cv2.morphologyEx(mask_image, cv2.MORPH_CLOSE, kernel, iterations=3)
 
     contours, _ = cv2.findContours(mask_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    pred_mask = np.zeros_like(mask_image)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if MIN_AREA < area < MAX_AREA:
+            cv2.drawContours(pred_mask, [contour], -1, (255, 0, 0), -1)
 
     image_with_contours = image.copy()
 
@@ -69,28 +80,35 @@ for file_path in tqdm(glob.glob(PATH + '/*.tif'), desc="Processing images"):
         if MIN_AREA < area < MAX_AREA:
             cv2.drawContours(image_with_contours, [contour], -1, (255, 0, 0), 1)
 
+    # Загрузка эталонной разметки
+    true_mask_path = PATH_CORES_CONTOURS + image_name + '_contours.png'
+    true_mask = cv2.imread(true_mask_path, cv2.IMREAD_GRAYSCALE)
+
+    if NEED_TEST:
+        if true_mask is not None:
+            dice_score, iou_score = calculate_metrics(pred_mask, true_mask)
+            if dice_score == 0 or iou_score == 0:
+                continue
+            total_dice += dice_score
+            total_iou += iou_score
+            processed_images += 1
+            print(f"{image_name} - Dice Score: {dice_score:.4f}, IoU Score: {iou_score:.4f}")
+
     if NEED_SHOW_RESULT:
+        contours, _ = cv2.findContours(true_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        image_true = image.copy()
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if MIN_AREA < area < MAX_AREA:
+                cv2.drawContours(image_true, [contour], -1, (255, 0, 0), 1)
 
-        # Пытаемся загрузить основное ядро и альтернативные версии с приписками "(1)" и "(2)"
-        file_path_core = PATH_CORES + file_path.split('/')[-1].replace('_S', '_N')
-        img_core = cv2.imread(file_path_core)
-
-        file_path_core_1 = file_path_core.replace('.tif', '(1).tif')
-        file_path_core_2 = file_path_core.replace('.tif', '(2).tif')
-
-        img_core_1 = cv2.imread(file_path_core_1) if os.path.exists(file_path_core_1) else None
-        img_core_2 = cv2.imread(file_path_core_2) if os.path.exists(file_path_core_2) else None
-
-        images_to_show = [image, segmented_image, image_with_contours, img_core, img_core_1, img_core_2]
-        titles = ['Исходное изображение', 'K-Means', 'Границы ядра', 'Основное ядро', 'Ядро (1)', 'Ядро (2)']
-
-        # Удаляем None из списка изображений и заголовков
-        images_to_show, titles = zip(*[(img, title) for img, title in zip(images_to_show, titles) if img is not None])
+        images_to_show = [image, segmented_image, image_with_contours, image_true]
+        titles = ['Исходное изображение', 'K-Means', 'Предсказание', 'Разметка']
 
         plt.figure(figsize=(15, 5))
         for i, (img, title) in enumerate(zip(images_to_show, titles), 1):
             plt.subplot(1, len(images_to_show), i)
-            plt.imshow(img)
+            plt.imshow(img, cmap='gray' if i > 1 else None)
             plt.title(title)
             plt.axis('off')
 
@@ -98,4 +116,11 @@ for file_path in tqdm(glob.glob(PATH + '/*.tif'), desc="Processing images"):
 
     if NEED_SAVE_RESULTS:
         os.makedirs(PATH_SAVE, exist_ok=True)
-        cv2.imwrite(f'{PATH_SAVE}/{image_name}.png', cv2.cvtColor(image_with_contours, cv2.COLOR_BGR2RGB))
+        cv2.imwrite(f'{PATH_SAVE}/{image_name}.png', pred_mask)
+
+if NEED_TEST:
+    if processed_images > 0:
+        mean_dice = total_dice / processed_images
+        mean_iou = total_iou / processed_images
+        print(f"\nСредний Dice Score: {mean_dice:.4f}")
+        print(f"Средний IoU Score: {mean_iou:.4f}")
